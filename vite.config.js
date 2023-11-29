16fed9b142db6023d8c2f6d27299fe52f38ecce0
rhicjoseph04 <142023096+rhicjoseph04@users.noreply.github.com>
2023-11-29 16:48:52 +0800

    addchanges

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://rhicjoseph04.github.io/Capstone_Project/', 
   plugins: [react()],
-})
+});
+
